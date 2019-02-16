6657ba9a8a320f995a4995e4c603d5fbd4cfc463
Kenneth Gomez <kenneth@Kenneths-MacBook-Pro-2.local>
2019-02-16 15:25:45 -0600

    our stuff

---
+++ bin/server/server.js
@@ -0,0 +1,237 @@
+/*jslint bitwise: true, node: true */
+'use strict';
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var SAT = require('sat');
+var sql = require("mysql");
+
+// Import game settings.
+var c = require('../../config.json');
+
+// Import utilities.
+var util = require('./lib/util');
+
+// Import quadtree.
+var quadtree = require('simple-quadtree');
+
+//call sqlinfo
+var s = c.sqlinfo;
+
+var tree = quadtree(0, 0, c.gameWidth, c.gameHeight);
+
+var users = [];
+var sockets = {};
+
+var V = SAT.Vector;
+var C = SAT.Circle;
+
+if (s.host !== "DEFAULT") {
+    var pool = sql.createConnection({
+        host: s.host,
+        user: s.user,
+        password: s.password,
+        database: s.database
+    });
+
+    //log sql errors
+    pool.connect(function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+var initMassLog = util.log(c.defaultPlayerMass, c.slowBase);
+
+app.use(express.static(__dirname + '/../client'));
+
+io.on('connection', function (socket) {
+    console.log('A user connected!', socket.handshake.query.type);
+
+    var type = socket.handshake.query.type;
+    var radius = util.massToRadius(c.defaultPlayerMass);
+    var position = c.newPlayerInitialPosition == 'farthest' ? util.uniformPosition(users, radius) : util.randomPosition(radius);
+
+    // if(type === 'player') {
+    //     cells = [{
+    //         mass: c.defaultPlayerMass,
+    //         x: position.x,
+    //         y: position.y,
+    //         radius: radius
+    //     }];
+    //     massTotal = c.defaultPlayerMass;
+    // }
+
+    var currentPlayer = {
+        id: socket.id,
+        x: position.x,
+        y: position.y
+    };
+
+    socket.on('gotit', function (player) {
+        console.log('[INFO] Player ' + player.name + ' connecting!');
+
+        if (util.findIndex(users, player.id) > -1) {
+            console.log('[INFO] Player ID is already connected, kicking.');
+            socket.disconnect();
+        } else if (!util.validNick(player.name)) {
+            socket.emit('kick', 'Invalid username.');
+            socket.disconnect();
+        } else {
+            console.log('[INFO] Player ' + player.name + ' connected!');
+            sockets[player.id] = socket;
+
+            var radius = util.massToRadius(c.defaultPlayerMass);
+            var position = c.newPlayerInitialPosition == 'farthest' ? util.uniformPosition(users, radius) : util.randomPosition(radius);
+
+            player.x = position.x;
+            player.y = position.y;
+            // player.target.x = 0;
+            // player.target.y = 0;
+            if (type === 'player') {
+                player.cells = [{
+                    mass: c.defaultPlayerMass,
+                    x: position.x,
+                    y: position.y,
+                    radius: radius
+                }];
+                player.massTotal = c.defaultPlayerMass;
+            } else {
+                player.cells = [];
+                player.massTotal = 0;
+            }
+            player.hue = Math.round(Math.random() * 360);
+            currentPlayer = player;
+            currentPlayer.lastHeartbeat = new Date().getTime();
+            users.push(currentPlayer);
+
+            io.emit('playerJoin', { name: currentPlayer.name });
+
+            socket.emit('gameSetup', {
+                gameWidth: c.gameWidth,
+                gameHeight: c.gameHeight
+            });
+            console.log('Total players: ' + users.length);
+        }
+    });
+
+    socket.on('pingcheck', function () {
+        socket.emit('pongcheck');
+    });
+
+    socket.on('windowResized', function (data) {
+        currentPlayer.screenWidth = data.screenWidth;
+        currentPlayer.screenHeight = data.screenHeight;
+    });
+
+    socket.on('respawn', function () {
+        if (util.findIndex(users, currentPlayer.id) > -1) users.splice(util.findIndex(users, currentPlayer.id), 1);
+        socket.emit('welcome', currentPlayer);
+        console.log('[INFO] User ' + currentPlayer.name + ' respawned!');
+    });
+
+    socket.on('disconnect', function () {
+        if (util.findIndex(users, currentPlayer.id) > -1) users.splice(util.findIndex(users, currentPlayer.id), 1);
+        console.log('[INFO] User ' + currentPlayer.name + ' disconnected!');
+
+        socket.broadcast.emit('playerDisconnect', { name: currentPlayer.name });
+    });
+
+    socket.on('playerChat', function (data) {
+        var _sender = data.sender.replace(/(<([^>]+)>)/ig, '');
+        var _message = data.message.replace(/(<([^>]+)>)/ig, '');
+        if (c.logChat === 1) {
+            console.log('[CHAT] [' + new Date().getHours() + ':' + new Date().getMinutes() + '] ' + _sender + ': ' + _message);
+        }
+        socket.broadcast.emit('serverSendPlayerChat', { sender: _sender, message: _message.substring(0, 35) });
+    });
+
+    socket.on('pass', function (data) {
+        if (data[0] === c.adminPass) {
+            console.log('[ADMIN] ' + currentPlayer.name + ' just logged in as an admin!');
+            socket.emit('serverMSG', 'Welcome back ' + currentPlayer.name);
+            socket.broadcast.emit('serverMSG', currentPlayer.name + ' just logged in as admin!');
+            currentPlayer.admin = true;
+        } else {
+
+            // TODO: Actually log incorrect passwords.
+            console.log('[ADMIN] ' + currentPlayer.name + ' attempted to log in with incorrect password.');
+            socket.emit('serverMSG', 'Password incorrect, attempt logged.');
+            pool.query('INSERT INTO logging SET name=' + currentPlayer.name + ', reason="Invalid login attempt as admin"');
+        }
+    });
+
+    socket.on('kick', function (data) {
+        if (currentPlayer.admin) {
+            var reason = '';
+            var worked = false;
+            for (var e = 0; e < users.length; e++) {
+                if (users[e].name === data[0] && !users[e].admin && !worked) {
+                    if (data.length > 1) {
+                        for (var f = 1; f < data.length; f++) {
+                            if (f === data.length) {
+                                reason = reason + data[f];
+                            } else {
+                                reason = reason + data[f] + ' ';
+                            }
+                        }
+                    }
+                    if (reason !== '') {
+                        console.log('[ADMIN] User ' + users[e].name + ' kicked successfully by ' + currentPlayer.name + ' for reason ' + reason);
+                    } else {
+                        console.log('[ADMIN] User ' + users[e].name + ' kicked successfully by ' + currentPlayer.name);
+                    }
+                    socket.emit('serverMSG', 'User ' + users[e].name + ' was kicked by ' + currentPlayer.name);
+                    sockets[users[e].id].emit('kick', reason);
+                    sockets[users[e].id].disconnect();
+                    users.splice(e, 1);
+                    worked = true;
+                }
+            }
+            if (!worked) {
+                socket.emit('serverMSG', 'Could not locate user or user is an admin.');
+            }
+        } else {
+            console.log('[ADMIN] ' + currentPlayer.name + ' is trying to use -kick but isn\'t an admin.');
+            socket.emit('serverMSG', 'You are not permitted to use this command.');
+        }
+    });
+});
+
+function movePlayer(player) {}
+
+function tickPlayer(currentPlayer) {
+    movePlayer(currentPlayer);
+}
+
+function moveloop() {
+    for (var i = 0; i < users.length; i++) {
+        tickPlayer(users[i]);
+    }
+}
+
+// function sendUpdates() {
+//     users.forEach( function(u) {
+//         sockets[u.id].emit('serverTellPlayerMove', visibleCells);
+//         if (leaderboardChanged) {
+//             sockets[u.id].emit('leaderboard', {
+//                 players: users.length,
+//                 leaderboard: leaderboard
+//             });
+//         }
+//     });
+//     leaderboardChanged = false;
+// }
+
+setInterval(moveloop, 1000 / 60);
+//setInterval(sendUpdates, 1000 / c.networkUpdateFactor);
+
+// Don't touch, IP configurations.
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || c.host;
+var serverport = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || c.port;
+http.listen(serverport, ipaddress, function () {
+    console.log('[DEBUG] Listening on ' + ipaddress + ':' + serverport);
+});
---
+++ src/client/js/app.js
@@ -0,0 +1,376 @@
+var io = require('socket.io-client');
+var ChatClient = require('./chat-client');
+var Canvas = require('./canvas');
+var global = require('./global');
+
+var playerNameInput = document.getElementById('playerNameInput');
+var socket;
+var reason;
+
+var debug = function(args) {
+    if (console && console.log) {
+        console.log(args);
+    }
+};
+
+if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+    global.mobile = true;
+}
+
+function startGame(type) {
+    global.playerName = playerNameInput.value.replace(/(<([^>]+)>)/ig, '').substring(0,25);
+    global.playerType = type;
+
+    // global.screenWidth = window.innerWidth;
+    // global.screenHeight = window.innerHeight;
+    global.screenWidth = 1400;
+    global.screenHeight = 750;
+
+    document.getElementById('startMenuWrapper').style.maxHeight = '0px';
+    document.getElementById('gameAreaWrapper').style.opacity = 1;
+    if (!socket) {
+        socket = io({query:"type=" + type});
+        setupSocket(socket);
+    }
+    if (!global.animLoopHandle)
+        animloop();
+    socket.emit('respawn');
+    window.chat.socket = socket;
+    window.chat.registerFunctions();
+    window.canvas.socket = socket;
+    global.socket = socket;
+}
+
+// Checks if the nick chosen contains valid alphanumeric characters (and underscores).
+function validNick() {
+    var regex = /^\w*$/;
+    debug('Regex Test', regex.exec(playerNameInput.value));
+    return regex.exec(playerNameInput.value) !== null;
+}
+
+window.onload = function() {
+
+    var btn = document.getElementById('startButton'),
+        btnS = document.getElementById('spectateButton'),
+        nickErrorText = document.querySelector('#startMenu .input-error');
+
+    btnS.onclick = function () {
+        startGame('spectate');
+    };
+
+    btn.onclick = function () {
+
+        // Checks if the nick is valid.
+        if (validNick()) {
+            nickErrorText.style.opacity = 0;
+            startGame('player');
+        } else {
+            nickErrorText.style.opacity = 1;
+        }
+    };
+
+    var settingsMenu = document.getElementById('settingsButton');
+    var settings = document.getElementById('settings');
+    var instructions = document.getElementById('instructions');
+
+    settingsMenu.onclick = function () {
+        if (settings.style.maxHeight == '300px') {
+            settings.style.maxHeight = '0px';
+        } else {
+            settings.style.maxHeight = '300px';
+        }
+    };
+
+    playerNameInput.addEventListener('keypress', function (e) {
+        var key = e.which || e.keyCode;
+
+        if (key === global.KEY_ENTER) {
+            if (validNick()) {
+                nickErrorText.style.opacity = 0;
+                startGame('player');
+            } else {
+                nickErrorText.style.opacity = 1;
+            }
+        }
+    });
+};
+
+function drawgrid() {
+    graph.lineWidth = 1;
+    graph.strokeStyle = global.lineColor;
+    graph.globalAlpha = 0.15;
+    graph.beginPath();
+    rows = 0;
+    cols = 0;
+
+   for (var x = 0; x < global.screenWidth; x += global.screenHeight / 18) {
+       graph.moveTo(x, 0);
+       graph.lineTo(x, global.screenHeight);
+       cols++;
+   }
+
+   for (var y = 0; y < global.screenHeight; y += global.screenHeight / 18) {
+       graph.moveTo(0, y);
+       graph.lineTo(global.screenWidth, y);
+       rows++;
+   }
+   global.rows = rows;
+   global.columns = cols;
+   graph.stroke();
+   graph.globalAlpha = 1;
+}
+
+function drawSquare(x, y, color){
+    graph.rect((x-1)*global.screenHeight/18, (y-1)*global.screenHeight/18 , global.screenHeight / 18, global.screenHeight / 18);
+    graph.stroke();
+    // graph.fillStyle = 'hsl(' + '#008080' + ', 100%, 50%)';
+    graph.fillStyle = '#008080';
+    graph.fill();
+
+}
+
+/////////////////////////////
+
+function gameLoop() {
+    if (global.died) {
+        graph.fillStyle = '#333333';
+        graph.fillRect(0, 0, global.screenWidth, global.screenHeight);
+
+        graph.textAlign = 'center';
+        graph.fillStyle = '#FFFFFF';
+        graph.font = 'bold 30px sans-serif';
+        graph.fillText('You died!', global.screenWidth / 2, global.screenHeight / 2);
+    }
+    else if (!global.disconnected) {
+        if (global.gameStart) {
+            graph.fillStyle = global.backgroundColor;
+            graph.fillRect(0, 0, global.screenWidth, global.screenHeight);
+
+            drawgrid();
+            drawSquare(10,10, "lol");
+
+            //users : list of players
+
+        } else {
+            graph.fillStyle = '#333333';
+            graph.fillRect(0, 0, global.screenWidth, global.screenHeight);
+
+            graph.textAlign = 'center';
+            graph.fillStyle = '#FFFFFF';
+            graph.font = 'bold 30px sans-serif';
+            graph.fillText('Game Over!', global.screenWidth / 2, global.screenHeight / 2);
+        }
+    } 
+    else {
+        graph.fillStyle = '#333333';
+        graph.fillRect(0, 0, global.screenWidth, global.screenHeight);
+
+        graph.textAlign = 'center';
+        graph.fillStyle = '#FFFFFF';
+        graph.font = 'bold 30px sans-serif';
+        if (global.kicked) {
+            if (reason !== '') {
+                graph.fillText('You were kicked for:', global.screenWidth / 2, global.screenHeight / 2 - 20);
+                graph.fillText(reason, global.screenWidth / 2, global.screenHeight / 2 + 20);
+            }
+            else {
+                graph.fillText('You were kicked!', global.screenWidth / 2, global.screenHeight / 2);
+            }
+        }
+        else {
+              graph.fillText('Disconnected!', global.screenWidth / 2, global.screenHeight / 2);
+        }
+    }
+}
+
+window.requestAnimFrame = (function() {
+    return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            window.msRequestAnimationFrame     ||
+            function( callback ) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+})();
+
+window.cancelAnimFrame = (function(handle) {
+    return  window.cancelAnimationFrame     ||
+            window.mozCancelAnimationFrame;
+})();
+
+function animloop() {
+    global.animLoopHandle = window.requestAnimFrame(animloop);
+    gameLoop();
+}
+
+var playerConfig = {
+    border: 6,
+    textColor: '#FFFFFF',
+    textBorder: '#000000',
+    textBorderSize: 3,
+    defaultSize: 30
+};
+
+var player = {
+    id: -1,
+    x: 0,
+    y: 0
+};
+global.player = player;
+
+var grid = [];
+for (let ii = 0; ii < global.rows; ii++){
+    grid.push([]);
+    for (let jj = 0; jj < global.columns; jj++){
+        grid[ii].push(undefined);
+    }
+}
+
+var users = [];
+window.canvas = new Canvas();
+var c = window.canvas.cv;
+var graph = c.getContext('2d');
+window.chat = new ChatClient();
+
+var visibleBorderSetting = document.getElementById('visBord');
+visibleBorderSetting.onchange = settings.toggleBorder;
+
+var showMassSetting = document.getElementById('showMass');
+showMassSetting.onchange = settings.toggleMass;
+
+var continuitySetting = document.getElementById('continuity');
+continuitySetting.onchange = settings.toggleContinuity;
+
+var roundFoodSetting = document.getElementById('roundFood');
+roundFoodSetting.onchange = settings.toggleRoundFood;
+
+// socket stuff.
+function setupSocket(socket) {
+    // Handle ping.
+    socket.on('pongcheck', function () {
+        var latency = Date.now() - global.startPingTime;
+        debug('Latency: ' + latency + 'ms');
+        window.chat.addSystemLine('Ping: ' + latency + 'ms');
+    });
+
+    // Handle error.
+    socket.on('connect_failed', function () {
+        socket.close();
+        global.disconnected = true;
+    });
+
+    socket.on('disconnect', function () {
+        socket.close();
+        global.disconnected = true;
+    });
+
+    // Handle connection.
+    socket.on('welcome', function (playerSettings) {
+        player = playerSettings;
+        player.name = global.playerName;
+        player.screenWidth = global.screenWidth;
+        player.screenHeight = global.screenHeight;
+        global.player = player;
+        socket.emit('gotit', player);
+        global.gameStart = true;
+        debug('Game started at: ' + global.gameStart);
+        window.chat.addSystemLine('Connected to the game!');
+        window.chat.addSystemLine('Type <b>-help</b> for a list of commands.');
+		c.focus();
+    });
+
+    socket.on('gameSetup', function(data) {
+        global.gameWidth = data.gameWidth;
+        global.gameHeight = data.gameHeight;
+        resize();
+    });
+
+    socket.on('playerDied', function (data) {
+        window.chat.addSystemLine('{GAME} - <b>' + (data.name.length < 1 ? 'An unnamed cell' : data.name) + '</b> was eaten.');
+    });
+
+    socket.on('playerDisconnect', function (data) {
+        window.chat.addSystemLine('{GAME} - <b>' + (data.name.length < 1 ? 'An unnamed cell' : data.name) + '</b> disconnected.');
+    });
+
+    socket.on('playerJoin', function (data) {
+        window.chat.addSystemLine('{GAME} - <b>' + (data.name.length < 1 ? 'An unnamed cell' : data.name) + '</b> joined.');
+    });
+
+    socket.on('serverMSG', function (data) {
+        window.chat.addSystemLine(data);
+    });
+
+    // Chat.
+    socket.on('serverSendPlayerChat', function (data) {
+        window.chat.addChatLine(data.sender, data.message, false);
+    });
+
+    // Handle movement.
+    socket.on('serverTellPlayerMove', function (userData) {
+        var playerData;
+        for(var i =0; i< userData.length; i++) {
+            if(typeof(userData[i].id) == "undefined") {
+                playerData = userData[i];
+                i = userData.length;
+            }
+        }
+        if(global.playerType == 'player') {
+            var xoffset = player.x - playerData.x;
+            var yoffset = player.y - playerData.y;
+
+            player.x = playerData.x;
+            player.y = playerData.y;
+            player.hue = playerData.hue;
+            player.massTotal = playerData.massTotal;
+            player.cells = playerData.cells;
+            player.xoffset = isNaN(xoffset) ? 0 : xoffset;
+            player.yoffset = isNaN(yoffset) ? 0 : yoffset;
+        }
+        users = userData;
+    });
+
+    // Death.
+    socket.on('RIP', function () {
+        global.gameStart = false;
+        global.died = true;
+        window.setTimeout(function() {
+            document.getElementById('gameAreaWrapper').style.opacity = 0;
+            document.getElementById('startMenuWrapper').style.maxHeight = '1000px';
+            global.died = false;
+            if (global.animLoopHandle) {
+                window.cancelAnimationFrame(global.animLoopHandle);
+                global.animLoopHandle = undefined;
+            }
+        }, 2500);
+    });
+
+    socket.on('kick', function (data) {
+        global.gameStart = false;
+        reason = data;
+        global.kicked = true;
+        socket.close();
+    });
+
+    socket.on('virusSplit', function (virusCell) {
+        socket.emit('2', virusCell);
+        reenviar = false;
+    });
+}
+
+
+window.addEventListener('resize', resize);
+
+function resize() {
+    if (!socket) return;
+
+    player.screenWidth = c.width = global.screenWidth = global.playerType == 'player' ? window.innerWidth : global.gameWidth;
+    player.screenHeight = c.height = global.screenHeight = global.playerType == 'player' ? window.innerHeight : global.gameHeight;
+
+    if (global.playerType == 'spectate') {
+        player.x = global.gameWidth / 2;
+        player.y = global.gameHeight / 2;
+    }
+
+    socket.emit('windowResized', { screenWidth: global.screenWidth, screenHeight: global.screenHeight });
+}
